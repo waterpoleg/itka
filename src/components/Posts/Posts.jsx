f837b8f5c173d9378c44aa62840c69d7e82ab939
Oleg Korneev <waterpoleg@gmail.com>
2021-08-04 00:46:33 +0300

    add func to button, refactor 31

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,19 +1,27 @@
 import Post from './Post/Post';
 import s from './Posts.module.css';
+import React from "react";
 
 const Posts = (props) => {
 
     let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>Posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add record</button>
+                    <button onClick={ addPost }>Add record</button>
                 </div>
             </div>
             <div className={s.posts}>
