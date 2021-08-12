0b3bbb66c72f3d5d6f344214ca0d43346f4e6470
Oleg Korneev <waterpoleg@gmail.com>
2021-08-12 18:05:49 +0300

    refactor 41

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,7 @@
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/state";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 
 const Dialogs = (props) => {
 
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,10 @@
 import Post from './Post/Post';
 import s from './Posts.module.css';
 import React from "react";
-import {addPostActionCreator, updateNewPostActionCreator} from "../../redux/state";
+import {addPostActionCreator, updateNewPostActionCreator} from "../../redux/profile-reducer";
 
 const Posts = (props) => {
-    let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     let newPostElement = React.createRef();
 
@@ -22,10 +22,10 @@ const Posts = (props) => {
             <h3>Posts</h3>
             <div>
                 <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
                 </div>
                 <div>
-                    <button onClick={ addPost }>Add record</button>
+                    <button onClick={addPost}>Add record</button>
                 </div>
             </div>
             <div className={s.posts}>
