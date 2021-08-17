0e378521a5a6ce19a204b20f7fa7b7205a2c28b9
Oleg Korneev <waterpoleg@gmail.com>
2021-08-18 00:53:20 +0300

    refactor 48++

--- src/components/Posts/PostsContainer.jsx
+++ src/components/Posts/PostsContainer.jsx
@@ -1,4 +1,3 @@
-import Post from './Post/Post';
 import React from "react";
 import {addPostActionCreator, updateNewPostActionCreator} from "../../redux/profile-reducer";
 import Posts from "./Posts";
