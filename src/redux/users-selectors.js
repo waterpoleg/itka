63146b2525cfdbc19d675b6d9ceac892abdaa097
Oleg Korneev <waterpoleg@gmail.com>
2021-09-01 22:45:19 +0300

    refactor 83 - reselect

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,13 @@
-export const getUsers = (state) => {
-    return state.usersPage.users.filter(u => true);
+import {createSelector} from "reselect";
+
+const getUsersSelector = (state) => {
+    return state.usersPage.users;
 }
+
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+        return users.filter(u => true);
+    })
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
