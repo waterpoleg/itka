cf4a5dc1dbdb8c768131b834f611bbb5913cbaa7
Oleg Korneev <waterpoleg@gmail.com>
2021-09-02 14:35:22 +0300

    refactor 85

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
-import s from './ProfileInfo.module.css';
+import React, {useEffect, useState} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
-
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
@@ -35,6 +37,5 @@ const ProfileStatusWithHooks = (props) => {
         </div>
     )
 }
-
 
 export default ProfileStatusWithHooks;
