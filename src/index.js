ec871d1e91548b74dbed0ee0c4bfa47e4e96d896
Oleg Korneev <waterpoleg@gmail.com>
2021-09-09 20:54:59 +0300

    refactor 91

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
-setInterval(() => {
-    store.dispatch({type: "FAKE"})
-}, 1000);
-
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
